468a9242342cb5288bc9dbc7e2a5edf858abb393
tetsuo <ogunduz@gmail.com>
2025-08-17 22:36:00 +0200

    Log enabled providers

--- main.go
+++ main.go
@@ -26,12 +26,14 @@ func main() {
 	if anthApiKey != "" {
 		c := anthropic.NewClient(anthropicoption.WithAPIKey(anthApiKey))
 		ac = &c
+		log.Print("enabling Anthropic models")
 	}
 
 	var oc *openai.Client
 	if openaiApiKey != "" {
 		c := openai.NewClient(openaioption.WithAPIKey(openaiApiKey))
 		oc = &c
+		log.Print("enabling OpenAI models")
 	}
 
 	server := &Server{
